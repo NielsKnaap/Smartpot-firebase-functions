f4fed3771d5628afc4b928d6ee253308184da7c8
Niels Knaap <niels@nielsknaap.nl>
2020-12-16 13:20:43 +0100

    [TASK] Save plant data

--- functions/lib/plant.js
+++ functions/lib/plant.js
@@ -1,3 +1,22 @@
 "use strict";
-// Silence is golden
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.functionAddPlant = void 0;
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+const USER_ID = 'k0eHgDhNhhWAb80CK8yKWIWr1zk2';
+exports.functionAddPlant = functions.https.onCall((data, context) => {
+    // if(!context.auth){
+    //     throw new functions.https.HttpsError(
+    //         'unauthenticated',
+    //         'You are not authenticated'
+    //     );
+    // }
+    return admin.firestore().collection('users').doc(USER_ID).collection('plants').add({
+        active: true,
+        light: 2,
+        soil: 1024,
+        temperature: 20
+    });
+});
 //# sourceMappingURL=plant.js.map
--- functions/src/plant.ts
+++ functions/src/plant.ts
@@ -1,1 +1,21 @@
-// Silence is golden
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+
+const USER_ID = 'k0eHgDhNhhWAb80CK8yKWIWr1zk2';
+
+export const functionAddPlant = functions.https.onCall((data, context) => {
+    // if(!context.auth){
+    //     throw new functions.https.HttpsError(
+    //         'unauthenticated',
+    //         'You are not authenticated'
+    //     );
+    // }
+    return admin.firestore().collection('users').doc(USER_ID).collection('plants').add({
+        active: true,
+        light: 2,
+        soil: 1024,
+        temperature: 20
+    });
+})
