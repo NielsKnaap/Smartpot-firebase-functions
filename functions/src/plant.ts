21b6b98bc0dd25ffe7544ff99c2d3667894d0402
Niels Knaap <niels@nielsknaap.nl>
2020-12-16 13:39:10 +0100

    [TASK] Makes it possible to add plants

--- functions/src/plant.ts
+++ functions/src/plant.ts
@@ -1,21 +1,17 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
-admin.initializeApp();
-
 const USER_ID = 'k0eHgDhNhhWAb80CK8yKWIWr1zk2';
 
-export const functionAddPlant = functions.https.onCall((data, context) => {
-    // if(!context.auth){
-    //     throw new functions.https.HttpsError(
-    //         'unauthenticated',
-    //         'You are not authenticated'
-    //     );
-    // }
-    return admin.firestore().collection('users').doc(USER_ID).collection('plants').add({
+export const functionAddPlant = functions.https.onRequest((request, response) => {
+    admin.firestore().collection('users').doc(USER_ID).collection('plants').add({
         active: true,
         light: 2,
         soil: 1024,
         temperature: 20
+    }).then( function (updatedPlant) {
+        response.send('Successfully added plant');
+    }).catch(function (error) {
+        response.send('Error while adding plant: ' + error);
     });
 })
