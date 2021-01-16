1a04c7b13bf4c2585da19c8bf79140bf314c8160
knap-development <niels@nielsknaap.nl>
2021-01-16 10:31:20 +0100

    [TASK] Add the function to query the latest measurements

--- functions/src/measurement.ts
+++ functions/src/measurement.ts
@@ -31,3 +31,19 @@ export const functionGetMeasurement = functions.https.onRequest((request, respon
             response.status(500).send('Error receiving measurement data');
         });
 });
+
+export const functionGetLastMeasurement = functions.https.onRequest((request, response) => {
+    var date = new Date();
+    date ;
+    date.setDate(date.getDate() - 7);
+
+    FIRESTORE.collection(USERS_COLLECTION).doc(request.body.userId).collection(PLANTS_COLLECTION).doc(request.body.plantId)
+        .collection(MEASUREMENTS_COLLECTION).where('timeStamp', '>', date).orderBy('timeStamp').get()
+        .then(snapshot => {
+            response.json(snapshot.docs.map(doc => doc.data()));
+        })
+        .catch(function (error) {
+            response.status(500).send('Error receiving measurement data');
+        });
+});
+
